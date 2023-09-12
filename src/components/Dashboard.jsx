1a45708ce07e013e0f93ee86f6e8d5d03ce197f4
Yimin Sheng <jamiesheng0306@gmail.com>
2023-09-12 15:57:52 -0400

    add select panel to select different models

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,4 +1,5 @@
 import {useEffect, useState} from 'react';
+import { Select } from '@chakra-ui/react'
 const Dashboard = (props) => {
   const [record,setRecord]=useState([]);
   useEffect(()=>{
@@ -15,11 +16,22 @@ const Dashboard = (props) => {
       </ul>
     )
   }
+
+  const renderOptions=(number)=>{
+    let options=[];
+    for(let i=0;i<number;i++){
+      options.push(<option value={i+1} key={i}>{i+1}</option>);
+    }
+    return options;
+  }
   return(
     <div style={{position:'absolute',top:0,right:0,width:'40%',height:'100%'}}>
     <h1>Dashboard</h1>
-    <p>Clicked</p>
+    <Select placeholder='Select option' onChange={(e)=>{props.selectHandler(e.target.value)}}>
+      {renderOptions(244)}
+    </Select>
     {addRecord()}
+    
     </div>
   )
 }
--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -4,23 +4,38 @@ import {dataProcess} from '../utils/dataWrangler';
 import CanvasWrapper from './CanvasWrapper';
 import Plot from './Plot';
 import Dashboard from './Dashboard';
+
 const Panel = () => {
   const [plotData,setPlotData]=useState([]);
   const [clicked,setClicked]=useState([0,0,0]);
+  const [selected,setSelected]=useState('1');
+  const [dataBys,setDataBys]=useState(new Map());
   useEffect(() => {
     d3.csv(process.env.PUBLIC_URL+'/fov011_AllFits.csv').then((data) => {
       const dataBys=d3.group(data,d=>d.s);
-      const ret=dataProcess(dataBys.get('17'));
+      setDataBys(dataBys);
+      //set default plot to be the first one
+      const ret=dataProcess(dataBys.get('1'));
       setPlotData(ret);
     }); 
   }, []); 
+
+  useEffect(()=>{
+    const ret=dataProcess(dataBys.get(selected));
+    setPlotData(ret);
+  },[selected]);
+
   const updateClicked=(point)=>{
     setClicked([point.x,point.y,point.z]);
   }
+  const updateSelected=(id)=>{
+    setSelected(id.toString());
+  }
+  
   return (
     <>
     <CanvasWrapper component={Plot} data={plotData} clickHandler={updateClicked}/>
-    <Dashboard clicked={clicked}/>
+    <Dashboard clicked={clicked} selectHandler={updateSelected}/>
     </>
   )
 }
