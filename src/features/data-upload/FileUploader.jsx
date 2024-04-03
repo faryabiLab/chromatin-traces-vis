34ff40a94b1daf9260f2afe8388a0039ddb6ffbb
Yimin Sheng <jamiesheng0306@gmail.com>
2024-04-03 16:22:03 -0400

    file uploader

--- src/features/data-upload/FileUploader.jsx
+++ src/features/data-upload/FileUploader.jsx
@@ -1,18 +1,18 @@
-import { Text,VStack} from '@chakra-ui/react';
+import { Divider, Text,VStack} from '@chakra-ui/react';
 
 
-import Instructions from './Instructions';
-import styles from '../Uploader.module.css';
+import Instructions from './components/Instructions';
+import styles from './Uploader.module.css';
 
-import DataBrowser from './DataBrowser';
-import CSVReader from './CSVReader';
+import DataBrowser from './components/DataBrowser';
+import CSVReader from './components/CSVReader';
 
 const FileUploader = () => {
 
   return (
     <VStack spacing='48px'>
       <Text
-        bgGradient="linear(to-l, blue.300,green.200)"
+        bgGradient="linear(to-l,green.400, blue.300)"
         bgClip="text"
         fontSize="6xl"
         fontWeight="extrabold"
@@ -25,6 +25,7 @@ const FileUploader = () => {
       <Instructions/>
       </div>
       <CSVReader />
+      <Divider />
       <DataBrowser />
     </VStack>
   );
