0c85a3def8dceebac6ffba9bd939fe7d82b4371d
Yimin Sheng <jamiesheng0306@gmail.com>
2025-03-19 10:41:59 -0400

    x/y axis

--- src/features/centrality/LinePlot.jsx
+++ src/features/centrality/LinePlot.jsx
@@ -1,5 +1,24 @@
 import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 import { calculateDistancesToCenter } from '../../utils/displayUtils';
+
+const CustomTooltip = ({ active, payload, label }) => {
+  if (active && payload && payload.length) {
+    return (
+      <div style={{
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        border: '1px solid #ccc',
+        padding: '10px',
+        borderRadius: '5px'
+      }}>
+        <p>{`Readout: ${label}`}</p>
+        <p>{ `Distance: ${payload[0].value} nm`}</p>
+      </div>
+    );
+  }
+
+  return null;
+};
+
 const LinePlot = ({data}) => {
   const distances = calculateDistancesToCenter(data);
   console.log(distances);
@@ -12,7 +31,7 @@ const LinePlot = ({data}) => {
           margin={{
             top: 30,
             right: 30,
-            left: 0,
+            left: 15,
             bottom: 20,
           }}
         >
@@ -21,14 +40,27 @@ const LinePlot = ({data}) => {
             dataKey="distance"
             stroke="#8884d8"
             strokeWidth={2}
-            name="Distance to Center"
+            name="Distance (nm)"
           />
           <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
           <XAxis 
             dataKey="readout" 
+            label={{ 
+              value: 'ORCA Readout', 
+              position: 'bottom',
+              
+            }}
           />
-          <YAxis/>
-        <Tooltip/>
+          <YAxis 
+          label={{ 
+              value: 'Distance to Geometric Center (nm)', 
+              angle: -90, 
+              position: 'insideLeft',
+              textAnchor: 'middle',
+              dy:150,
+              dx:-10,
+            }}/>
+        <Tooltip content={<CustomTooltip />}/>
         </LineChart>
 
     </div>
