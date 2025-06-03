82a9e6f179b8d3e5a0c72f45abc1aef70a9cd82e
Yimin Sheng <jamiesheng0306@gmail.com>
2025-06-03 13:03:06 -0400

    adjust layout

--- src/features/data-upload/Landing.jsx
+++ src/features/data-upload/Landing.jsx
@@ -6,7 +6,7 @@ import { User, Rat, Bug } from 'lucide-react';
 
 const Landing = () => {
   const [selectedSpecies, setSelectedSpecies] = useState('');
-  
+
   return (
     <VStack spacing="24px" marginBottom={'100px'}>
       <Box bgGradient={'linear( #E2F4C5 5%, yellow.50 60%, white 100%)'} w="100vw" py={10}>
@@ -21,109 +21,111 @@ const Landing = () => {
           Optical Looping Interactive Viewing Engine (OLIVE)
         </Text>
       </Box>
-      <Box width="60%">
-      <Link
-      href="https://github.com/faryabiLab/chromatin-traces-vis" // Replace with your actual GitHub URL
-      isExternal // This will open in a new tab
-      _hover={{ textDecoration: 'none' }} // Removes default link underline
-    >
-      <Box 
-        p={4} 
-        borderRadius="md" 
-        width="100%" 
-        textAlign="center"
-        border="4px"
-        borderColor="#E7EFC7"
-        cursor="pointer"
-        _hover={{ bg: '#E7EFC7' }}
-      >
-        <Text as="b" fontSize="lg" color="teal.700">
-          First time to OLIVE? Click here to visit our github page for more information!
-        </Text>
+      <Box width="80%">
+        <Link
+          href="https://github.com/faryabiLab/chromatin-traces-vis" // Replace with your actual GitHub URL
+          isExternal // This will open in a new tab
+          _hover={{ textDecoration: 'none' }} // Removes default link underline
+        >
+          <Box
+            p={4}
+            borderRadius="md"
+            width="100%"
+            textAlign="center"
+            border="4px"
+            borderColor="#E7EFC7"
+            cursor="pointer"
+            _hover={{ bg: '#E7EFC7' }}
+          >
+            <Text as="b" fontSize="lg" color="teal.700">
+              First time to OLIVE? Click here to visit our github page for more information!
+            </Text>
+          </Box>
+        </Link>
       </Box>
-    </Link>
-    </Box>
       {!selectedSpecies && (
-        <Flex gap={8} width="60%" justifyContent="center">
-          <Box width="50%">
-            <VStack align="start" spacing={6}>
-              <Text as="b" fontSize="3xl">
-                Browse Available Chromatin Traces
-              </Text>
-              <Flex direction="column" gap={4} width="100%">
-                <Button
-                  onClick={() => setSelectedSpecies('human')}
-                  height="120px"
-                  width="60%"
-                  colorScheme="teal"
-                  variant={selectedSpecies === 'human' ? 'solid' : 'outline'}
-                  display="flex"
-                  gap={4}
-                  justifyContent="flex-start"
-                  padding={6}
-                >
-                  <User size={32} />
-                  <VStack align="start" spacing={0}>
-                    <Text fontSize="2xl">Human</Text>
-                    
-                  </VStack>
-                </Button>
+        <Flex gap={4} width="80%" margin="0 auto">
+          <Flex width="100%" justifyContent="center">
+            <Box width="100%">
+              <VStack align="center" spacing={4}>
+                <Text as="b" fontSize="3xl">
+                  Browse Available Chromatin Traces
+                </Text>
+                <Flex direction="column" gap={4} width="100%" align="center">
+                  <Button
+                    onClick={() => setSelectedSpecies('human')}
+                    height="150px"
+                    width="60%"
+                    colorScheme="teal"
+                    variant={selectedSpecies === 'human' ? 'solid' : 'outline'}
+                    display="flex"
+                    gap={4}
+                    justifyContent="center"
+                    padding={6}
+                  >
+                    <User size={32} />
+                    <VStack align="start" spacing={0}>
+                      <Text fontSize="2xl">Human</Text>
+                    </VStack>
+                  </Button>
 
-                <Button
-                  onClick={() => setSelectedSpecies('mouse')}
-                  height="120px"
-                  width="60%"
-                  colorScheme="teal"
-                  variant={selectedSpecies === 'mouse' ? 'solid' : 'outline'}
-                  display="flex"
-                  gap={4}
-                  justifyContent="flex-start"
-                  padding={6}
-                >
-                  <Rat size={32} />
-                  <VStack align="start" spacing={0}>
-                    <Text fontSize="2xl">Mouse</Text>
-              
-                  </VStack>
-                </Button>
+                  <Button
+                    onClick={() => setSelectedSpecies('mouse')}
+                    height="150px"
+                    width="60%"
+                    colorScheme="teal"
+                    variant={selectedSpecies === 'mouse' ? 'solid' : 'outline'}
+                    display="flex"
+                    gap={4}
+                    justifyContent="center"
+                    padding={6}
+                  >
+                    <Rat size={32} />
+                    <VStack align="start" spacing={0}>
+                      <Text fontSize="2xl">Mouse</Text>
+                    </VStack>
+                  </Button>
 
-                <Button
-                  onClick={() => setSelectedSpecies('fly')}
-                  height="120px"
-                  width="60%"
-                  colorScheme="teal"
-                  variant={selectedSpecies === 'fly' ? 'solid' : 'outline'}
-                  display="flex"
-                  gap={4}
-                  justifyContent="flex-start"
-                  padding={6}
-                >
-                  <Bug size={32} />
-                  <VStack align="start" spacing={0}>
-                    <Text fontSize="2xl">Fly</Text>
-                  
-                  </VStack>
-                </Button>
-              </Flex>
-            </VStack>
-          </Box>
-          <Box width="50%">
-            <Uploader />
-          </Box>
+                  <Button
+                    onClick={() => setSelectedSpecies('fly')}
+                    height="150px"
+                    width="60%"
+                    colorScheme="teal"
+                    variant={selectedSpecies === 'fly' ? 'solid' : 'outline'}
+                    display="flex"
+                    gap={4}
+                    justifyContent="center"
+                    padding={6}
+                  >
+                    <Bug size={32} />
+                    <VStack align="start" spacing={0}>
+                      <Text fontSize="2xl">Fly</Text>
+                    </VStack>
+                  </Button>
+                </Flex>
+              </VStack>
+            </Box>
+          </Flex>
+
+          <Flex width="100%" justifyContent="center">
+            <Box width="100%">
+              <Uploader />
+            </Box>
+          </Flex>
         </Flex>
       )}
       {selectedSpecies && (
         <>
-          <Button 
-            colorScheme="teal" 
-            variant="outline" 
+          <Button
+            colorScheme="teal"
+            variant="outline"
             onClick={() => setSelectedSpecies('')}
             ml={8}
           >
             ← Back
           </Button>
           <Divider />
-          <DataBrowser species={selectedSpecies}/>
+          <DataBrowser species={selectedSpecies} />
         </>
       )}
     </VStack>
--- src/features/data-upload/Uploader.jsx
+++ src/features/data-upload/Uploader.jsx
@@ -24,6 +24,7 @@ const Uploader = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <Box width="100%">
+      <VStack align="center" spacing={5}>
       <VStack align="start" spacing={5}>
         <Text as="b" fontSize="3xl">
           Upload your own chromatin traces
@@ -31,119 +32,125 @@ const Uploader = () => {
         <Text as="i" fontSize="xl">
           Required Columns
         </Text>
+        </VStack>
         <Modal isOpen={isOpen} onClose={onClose}>
           <ModalOverlay />
           <ModalContent>
             <ModalHeader>Upload Files</ModalHeader>
             <ModalCloseButton />
             <ModalBody pb={6}>
-              <CSVReader/>
+              <CSVReader />
             </ModalBody>
           </ModalContent>
         </Modal>
-        <TableContainer>
-          <Table
-            variant="simple"
-            sx={{
-              th: {
-                backgroundColor: '#6A9C89',
-                color: 'white',
-                fontSize: 'lg',
-              },
-              // Make borders more visible
-              'th, td': {
-                borderColor: 'rgba(72, 113, 109, 0.5)',
-              },
-            }}
-          >
-            {/* <TableCaption>Requried Columns</TableCaption> */}
-            <Thead>
-              <Tr>
-                <Tooltip label="Field of View" placement="top">
-                  <Th fontSize="lg">FOV</Th>
-                </Tooltip>
-                <Tooltip label="Allele number of a trace" placement="top">
-                  <Th fontSize="lg">Trace (s)</Th>
-                </Tooltip>
-                <Tooltip label="Sequential order of a registered step of a trace" placement="top">
-                  <Th fontSize="lg">Readout</Th>
-                </Tooltip>
-                <Tooltip label="X-axis coordinate" placement="top">
-                  <Th fontSize="lg">X</Th>
-                </Tooltip>
-                <Tooltip label="Y-axis cooordinate" placement="top">
-                  <Th fontSize="lg">Y</Th>
-                </Tooltip>
-                <Tooltip label="Z-axis coordinate" placement="top">
-                  <Th fontSize="lg">Z</Th>
-                </Tooltip>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td></Td>
-                <Td></Td>
-                <Td></Td>
-                <Td></Td>
-                <Td></Td>
-                <Td></Td>
-              </Tr>
-              <Tr>
-                <Td></Td>
-                <Td></Td>
-                <Td></Td>
-                <Td></Td>
-                <Td></Td>
-                <Td></Td>
-              </Tr>
-
-              <Tr>
-                <Td></Td>
-
-                <Td
-                  rowSpan={3}
-                  colSpan={4}
-                  textAlign={'center'}
-                  onClick={onOpen}
-                  cursor="pointer"
-                  _hover={{ bg: 'gray.50' }}
-                >
-                  <Text as="b" fontSize="xl">
-                    Click to start uploading
-                  </Text>
-                </Td>
-
-                <Td></Td>
-              </Tr>
-              <Tr>
-                <Td></Td>
-
-                <Td></Td>
-              </Tr>
-              <Tr>
-                <Td></Td>
-
-                <Td></Td>
-              </Tr>
-              <Tr>
-                <Td></Td>
-                <Td></Td>
-                <Td></Td>
-                <Td></Td>
-                <Td></Td>
-                <Td></Td>
-              </Tr>
-              <Tr>
-                <Td></Td>
-                <Td></Td>
-                <Td></Td>
-                <Td></Td>
-                <Td></Td>
-                <Td></Td>
-              </Tr>
-            </Tbody>
-          </Table>
-        </TableContainer>
+        <Box overflowX="auto">
+          <TableContainer minWidth="600px" maxWidth="100%">
+            <Table
+              variant="simple"
+              size={{ base: 'sm', md: 'md', lg: 'lg' }} // Responsive table size
+              sx={{
+                th: {
+                  backgroundColor: '#6A9C89',
+                  color: 'white',
+                  fontSize: { base: 'md', lg: 'lg' }, // Responsive font size
+                  whiteSpace: 'nowrap', // Prevents text wrapping in headers
+                  px: { base: 2, md: 4 }, // Responsive padding
+                },
+                'th, td': {
+                  borderColor: 'rgba(72, 113, 109, 0.5)',
+                  px: { base: 2, md: 4 }, // Responsive padding
+                },
+                td: {
+                  fontSize: { base: 'sm', md: 'md', lg: 'lg' }, // Responsive font size
+                },
+              }}
+            >
+              <Thead>
+                <Tr>
+                  <Tooltip label="Field of View" placement="top">
+                    <Th>FOV</Th>
+                  </Tooltip>
+                  <Tooltip label="Allele number of a trace" placement="top">
+                    <Th>Trace (s)</Th>
+                  </Tooltip>
+                  <Tooltip label="Sequential order of a registered step of a trace" placement="top">
+                    <Th>Readout</Th>
+                  </Tooltip>
+                  <Tooltip label="X-axis coordinate" placement="top">
+                    <Th>X</Th>
+                  </Tooltip>
+                  <Tooltip label="Y-axis cooordinate" placement="top">
+                    <Th>Y</Th>
+                  </Tooltip>
+                  <Tooltip label="Z-axis coordinate" placement="top">
+                    <Th>Z</Th>
+                  </Tooltip>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td></Td>
+                  <Td></Td>
+                  <Td></Td>
+                  <Td></Td>
+                  <Td></Td>
+                  <Td></Td>
+                </Tr>
+                <Tr>
+                  <Td></Td>
+                  <Td></Td>
+                  <Td></Td>
+                  <Td></Td>
+                  <Td></Td>
+                  <Td></Td>
+                </Tr>
+                <Tr>
+                  <Td></Td>
+                  <Td
+                    rowSpan={3}
+                    colSpan={4}
+                    textAlign={'center'}
+                    onClick={onOpen}
+                    cursor="pointer"
+                    _hover={{ bg: 'gray.50' }}
+                  >
+                    <Text
+                      as="b"
+                      fontSize={{ base: 'lg', md: 'xl' }} // Responsive font size
+                    >
+                      Click to start uploading
+                    </Text>
+                  </Td>
+                  <Td></Td>
+                </Tr>
+                <Tr>
+                  <Td></Td>
+                  <Td></Td>
+                </Tr>
+                <Tr>
+                  <Td></Td>
+                  <Td></Td>
+                </Tr>
+                <Tr>
+                  <Td></Td>
+                  <Td></Td>
+                  <Td></Td>
+                  <Td></Td>
+                  <Td></Td>
+                  <Td></Td>
+                </Tr>
+                <Tr>
+                  <Td></Td>
+                  <Td></Td>
+                  <Td></Td>
+                  <Td></Td>
+                  <Td></Td>
+                  <Td></Td>
+                </Tr>
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </Box>
       </VStack>
     </Box>
   );
